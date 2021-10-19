a6becf832b4837b4f0f5fefd0d42b6f3bb63d128
Michael Stevens <michaeljstevens443@gmail.com>
2021-10-19 17:56:27 -0400

    Hello Kotlin: First push (the basics)

---
+++ app/src/main/java/com/example/hellokotlin/HelloWorld.kt
@@ -0,0 +1,37 @@
+package com.example.hellokotlin
+
+import java.util.*
+
+//Get Familiar with Kotlin
+
+fun printMessage(message: String): Unit {
+    println(message)
+}
+
+fun printMessageWithPrefix(message: String, prefix: String = "Info") {
+    println("[$prefix] $message")
+}
+
+fun sum(x: Int, y: Int): Int {
+    return x + y
+}
+
+fun multiply(x: Int, y: Int): Int {
+    return x * y
+}
+
+
+fun main() {
+    println("Hello, World!")
+    printMessage("lol test")
+    printMessageWithPrefix("message");
+    printMessageWithPrefix("message", "prefix is changed");
+    println(sum(4,5))
+    println(multiply(2,3))
+    val read = Scanner(System.`in`)
+    println("Enter an integer to sum: ")
+    val num1 = read.nextInt()
+    println("Enter another integer to sum: ")
+    val num2 = read.nextInt()
+    println("Summation: " + sum(num1, num2))
+}
